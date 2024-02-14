1138b562e3201584075f7caf646397822d75772d
dragos-gheorghioiu <dragos.gheorghioiu@vodafone.com>
2024-02-14 15:04:41 +0200

    comments and harpoon

--- init.lua
+++ init.lua
@@ -62,3 +62,31 @@ vim.keymap.set("n", "<Leader>pch", "<cmd>ClangdSwitchSourceHeader<cr>")
 require("autocommand")
 require("nvim-autopairs").setup({})
 require("formatter-nvim")
+
+-- ##############################Harpoon##############################
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>ha", function()
+	harpoon:list():append()
+end)
+vim.keymap.set("n", "<leader>hl", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<leader>h1", function()
+	harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<leader>h2", function()
+	harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<leader>h3", function()
+	harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<leader>h4", function()
+	harpoon:list():select(4)
+end)
+-- ##############################Harpoon##############################
