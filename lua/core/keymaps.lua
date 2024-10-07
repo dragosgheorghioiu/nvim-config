19e9620fa37a019cf83662c21d60b178d560ef1c
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2024-10-07 14:30:10 +0300

    telescope and oil hidden file support

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,5 +3,7 @@ local M = {
 	vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>"),
 	-- for C/C++ change between source file and header
 	vim.keymap.set("n", "<Leader>pch", "<cmd>ClangdSwitchSourceHeader<cr>"),
+	-- Oil remap
+	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
 }
 return M
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,23 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function ()
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-		require("oil").setup({})
-	end,
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+		columns = {
+			"icon",
+			"permissions",
+			"size",
+		},
+		keymaps = {
+			["<C-s>"] = false,
+			["<C-h>"] = false,
+			["<C-t>"] = false,
+			["<C-p>"] = false,
+			["<C-c>"] = false,
+			["<C-l>"] = false,
+			["<C-r>"] = "actions.refresh",
+		},
+	},
 }
