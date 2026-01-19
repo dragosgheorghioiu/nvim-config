fe7d7bf87b459ab8c9d4b2b26f056d53b56b9a98
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2026-01-19 14:06:42 +0200

    c toggle between source and header files

---
+++ after/ftplugin/c.lua
@@ -0,0 +1,17 @@
+vim.keymap.set("n", "<leader>ch", function()
+  local file_path = vim.fn.expand('%:p')
+  local ext = vim.fn.expand('%:e')
+  local new_path = nil
+
+  if ext == 'c' then
+    new_path = file_path:gsub('%.c$', '.h')
+  elseif ext == 'h' then
+    new_path = file_path:gsub('%.h$', '.c')
+  else
+    print("Not a .c or .h file")
+    return
+  end
+
+  vim.cmd('edit ' .. new_path)
+end, { desc = "Switch between source and header" })
+
